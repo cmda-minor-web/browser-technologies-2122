cda0526d3ec56197a79f6630dc7546efc42bae20
jornveltrop <jorn1veltrop@gmail.com>
2022-04-06 17:38:56 +0200

    set-up slider

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,21 @@
 function init() {
-
+    setMenuActive();
 }
 
 init();
+
+
+function setMenuActive() {
+    if (document.body.classList) {
+        if (window.location.pathname == "/") {
+            const menuItemOne = document.querySelector('nav a:nth-child(1)')
+            menuItemOne.classList.add('active');
+        } else if (window.location.pathname == "/resultaten") {
+            const menuItemTwo = document.querySelector('nav a:nth-child(2)')
+            menuItemTwo.classList.add('active');
+        } else if (window.location.pathname == "/admin") {
+            const menuItemThree = document.querySelector('nav a:nth-child(3)')
+            menuItemThree.classList.add('active');
+        }
+    }
+}
